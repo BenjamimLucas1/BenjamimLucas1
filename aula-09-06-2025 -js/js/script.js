9fbd180eee2c8028326f57afdd0ed018f96dc423
BenjamimLucas1 <nataliaariadnegs@hotmail.com>
2026-06-09 17:23:58 -0300

    Add files via upload

---
+++ aula-09-06-2025 -js/js/script.js
@@ -0,0 +1,173 @@
+/*
+let numero = Number(prompt("Informe a sua nota:"))
+if(nota >= 90){
+    alert("O conceito é A")
+}
+else if(nota >= 80){
+    alert("O concento é B")
+}
+else{
+    alert("O conceito é C")
+}
+
+*/
+
+/*
+let numero = Number(prompt("Informe um número de 1 a 7:"))
+switch(numero){
+    case 1 : alert("Domingo"); break;
+    case 2 : alert("Segunda-feira"); break;
+    case 3 : alert("Terça-feira"); break;
+    case 4 : alert("Quarta-feira"); break;
+    case 5 : alert("Quinta-feira"); break;
+    case 6 : alert("Sexta-feira"); break;
+    case 7 : alert("Sábado"); break;
+    default : alert("Número inválido"); break;
+}
+
+*/
+
+/*
+let temperatura = Number(prompt("Informe a temperatura"))
+let temp = temperatura >= 25 ? "Quente" : "Frio";
+alert(temp)
+
+*/
+
+/*
+let maiorIdade = prompt("você é maior de idade ?") === "true"
+let carteira = prompt("Tem carteira de motorista") === "true"
+if(maiorIdade && carteira){
+    alert("Pode dirigir")
+}
+else{
+    alert("Proibido dirigir")
+}
+
+*/
+
+/*
+let numero = Number(prompt("Informe um número"))
+if(numero >= 10 && numero <= 20){
+    alert("Este número está entre 10 e 20")
+}
+else{
+    alert("Esse número não está entre 10 e 20")
+}
+
+*/
+
+/*
+let sinal = prompt("Informe a cor do semáforo").toLowercase()
+switch(sinal){
+    case "vermelho" : alert("Pare"); break;
+    case "amarelo" : alert("Atenção! Começe a freiar"); break;
+    case "verde" : alert("Pode seguir")
+    default : alert("Essa cor não é valida"); break;
+}
+
+*/
+
+/**/ 
+
+/*
+let idade = prompt("Digite a sua idade para ver se você pode assistir ao filme")
+if(idade >= 18){
+    alert("Pode assistir ao filme")
+}
+else if(idade >= 16){
+    alert("Pde assistir, mas precisa de uma declaração com uma autorização dos pais")
+}
+else{
+    alert("Não pode assisitr")
+}
+
+*/
+
+/*
+let pets = prompt("Indique a quantidade de pets que você tem")
+switch(pets){
+    case "0" : alert("Você não possui pets ? Tudo bem, muitas pessoas também não tem")
+    case "1" : alert("Você tem 1 pet. Isso é bem comum")
+    case "2" : alert("Caramba, 2 pets em! Você gosta muito de bixinhos")
+    case "3" : alert("Olha, estou impressionado. Você deve amar e cuidar muito bem deles ...")
+    default  : alert("Você é uma ONG, não é possível")
+
+}
+
+*/
+
+/*
+let numero = prompt("Digite um número inteiro não nulo e eu vou te dizer se esse número é positivo ou negativo")
+let resposta = numero > 0 ? "O número que você digitou é positivo" : "O número que você digitou é negativo"
+alert(resposta)
+
+*/
+
+/*
+let inteligencia = prompt("Você é inteligente e/ou estuda ?") === "sim"
+let gosta = prompt("Você gosta de avião, ou mesmo sem gostar trabalharia como piloto ?") === "sim"
+if(inteligencia && gosta){
+    alert("Você pode ser piloto de avião")
+}
+else{
+    alert("Não indico você a ser piloto de avião")
+}
+
+*/
+
+/*
+let num1 = Number(prompt("Digite um número"))
+let num2 = Number(prompt("Digite outro número"))
+if(num1 > num2){
+    alert("O primeiro número é maior que o segundo")
+}
+else if(num1 < num2){
+    alert("O segundo número é maior que o primeiro")
+}
+else{
+    alert("Os números tem o mesmo valor")
+}
+
+*/
+
+/*
+let sexo = prompt("Digite o seu sexo, sendo 'm' para masculino e 'f' para feminino").toLowerCase()
+switch(sexo){
+    case "m" : alert("masculino"); break;
+    case "f" : alert("feminino"); break;
+    default : alert("Seu sexo não EXISTE")
+}
+
+*/
+
+/*
+let letra = prompt("Digite apenas uma letra").toLowerCase()
+switch(letra){
+    case "a" : alert("vogal"); break;
+    case "e" : alert("vogal"); break;
+    case "i" : alert("vogal"); break;
+    case "o" : alert("vogal"); break;
+    case "u" : alert("vogal"); break;
+    default : alert("consoante"); break;
+}
+
+*/
+
+/*
+let nota1 = Number(prompt("Digite sua primeira nota"))
+let nota2 = Number(prompt("Digite sua segunda nota"))
+let media = (nota1 + nota2)/2
+if(media == 10){
+    alert("Aprovado com ")
+}
+else if(media >= 7){
+    alert("Aprovado")
+}
+else{
+    alert("Reprovado")
+}
+
+*/
+
+
